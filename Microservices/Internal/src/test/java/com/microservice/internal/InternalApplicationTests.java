171d9efe97d7ac42f67c8b0365924e75b42418f6
Prakhyat Shrestha <81764309+prakh-yat@users.noreply.github.com>
2023-06-22 13:21:26 +0545

    Added

---
+++ Microservices/Internal/src/test/java/com/microservice/internal/InternalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.internal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
